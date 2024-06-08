ce2f0d7e62d5a0cd5e9fbee3cc85a621e1a884eb
Muhammad Rafindha Aslam <97833747+Rfndhaslam@users.noreply.github.com>
2024-06-08 23:02:19 +0700

    Delete bandungpariwisataapp directory

--- bandungpariwisataapp/DestinationImages.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bandungpariwisataapp.models
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class DestinationImages(
-    val images: List<String>
-) : Parcelable
